7d74c6223514eb593146174ed6b1ea79ec136192
Robert Edwardes <rdedwardes@gmail.com>
2024-01-17 18:40:52 -0500

    final tweak before deploy

--- app.py
+++ app.py
@@ -90,10 +90,10 @@ def job1():
             db.session.add(last_time)
             db.session.commit()
     
-@scheduler.task('interval', id='do_job_2', hours=2, misfire_grace_time=1900)
+@scheduler.task('interval', id='do_job_2', hours=1, misfire_grace_time=1900)
 def job2():
     with scheduler.app.app_context():
-        N_hours_ago = datetime.now() - timedelta(hours=46)
+        N_hours_ago = datetime.now() - timedelta(hours=48)
         db.session.query(MyModel).filter(MyModel.created_at < N_hours_ago).delete()
         db.session.commit()
         print('job2')
